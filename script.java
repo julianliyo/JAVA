bb6b434abb2d9fdb52f63b4b6a7cb188d8741ffb
Julian Albornoz <129104628+julianliyo@users.noreply.github.com>
2023-07-19 19:51:40 -0300

    Add files via upload

---
+++ script.java
@@ -0,0 +1,8 @@
+public class script {
+    public static void main(String[] args) {
+    System.out.println("Mi nombre es julian");
+    System.out.println("Tengo 18 años de edad");
+    System.out.println("Vivo en argentina");            
+    }
+}
+
