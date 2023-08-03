b6db2674713dd8b75fb9fd36af2a026fa05ba1cc
Julian Albornoz <129104628+julianliyo@users.noreply.github.com>
2023-08-03 19:21:29 -0300

    Add files via upload

--- proyectospring/src/main/java/com/julian/proyectospring/ProyectospringApplication.java
+++ proyectospring/src/main/java/com/julian/proyectospring/ProyectospringApplication.java
@@ -2,8 +2,10 @@ package com.julian.proyectospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 
@@ -12,19 +14,40 @@ public class ProyectospringApplication {
         public static void main(String[] args) {
                 SpringApplication.run(ProyectospringApplication.class, args);
         }
-        
-        // 1. Anotación
-        @RequestMapping("/")
-        // 3. Método que se asigna a la Solicitud anterior.
-        public String hello() { // 3
-                return "Hola, como estas cliente?";
-        }
-        
-        // 1. Anotación
-        @RequestMapping("/random")
-        // 3. Método que se asigna a la Solicitud anterior.
-        public String springbot() { // 3
-                return "Spring boot es muy bueno, y es facil de utilizar";
-        }
+
+                @RequestMapping("/coding")
+
+                    @GetMapping
+                    public String helloCodingDojo() {
+                        return "¡Hola Coding Dojo!";
+                    }
+
+                    @GetMapping("/coding/python")
+                    public String pythonCodingDojo() {
+                        return "¡Python/Django fue increíble!";
+                    }
+
+                    @GetMapping("/coding/java")
+                    public String javaCodingDojo() {
+                        return "¡Java/Spring es mejor!";
+                    }
+                   
+                    @RestController
+                    public class DojoController {  
+                    
+                        @GetMapping("/{dojoNombre}")
+                        public String getDojoByName(@PathVariable String dojoNombre) {
+                            switch (dojoNombre) {
+                            case "dojo":
+                            	return "¡El Dojo es increíble!";
+                                case "burbank-dojo":
+                                    return "El Dojo Burbank está localizado al sur de California.";
+                                case "san-jose":
+                                    return "El Dojo SJ es el cuartel general.";
+                            }
+							return dojoNombre;
+                        }
+                    }
 }
+
 
